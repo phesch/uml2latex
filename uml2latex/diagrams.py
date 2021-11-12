f19dc447687caa532935491ea1e46c1880972123
phesch <phesch@phesch.de>
2021-11-12 22:27:08 +0100

    Add the GNU GPLv3 license.

--- uml2latex/diagrams.py
+++ uml2latex/diagrams.py
@@ -1,5 +1,22 @@
 # vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
 
+# Copyright 2021 phesch <phesch@phesch.de>
+
+# This file is part of uml2latex.
+#
+# uml2latex is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# uml2latex is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with uml2latex.  If not, see <https://www.gnu.org/licenses/>.
+
 """Code for generating diagrams containing only a single class."""
 
 import xml.etree.ElementTree as ET
